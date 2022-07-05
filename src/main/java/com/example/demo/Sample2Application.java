4d660e711b07c7d56164efb7e89bdb3a4d8d8a6e
NamburiBharath <namburibharath50959@gmail.com>
2022-07-05 20:16:35 +0530

    first scripted

---
+++ src/main/java/com/example/demo/Sample2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample2Application.class, args);
+		System.out.println("heeeu");
+	}
+
+}
